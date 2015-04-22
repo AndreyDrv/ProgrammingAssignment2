12fd0bc74bb705c3147157763d59f0dbd4cfe442
Andrey Glushkov <andrey.drv@gmail.com>
2015-04-22 12:03:33 +0900

    matrix solver cache added with simple test case
    run cacheSolveTest()

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,42 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  
+  ## Return a matrix that is the inverse of 'x'
+  m
+}
+
+cacheSolveTest <- function(matrix = matrix( c(2, 4, 3,  1, 5, 7,  7, 4, 1), nrow=3, ncol=3 )) {  
+  message("cache = makeCacheMatrix(x)   #initializing cache")
+  cache = makeCacheMatrix(x)
+  message("cacheSolve(cache)            #first run: adding data to cache")
+  cacheSolve(cache)
+  message("cacheSolve(cache)            #second run: data from cache is returned")
+  cacheSolve(cache)
 }
